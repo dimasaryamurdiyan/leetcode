7902e230a450a3c1c93ae6ae9074859163e45aa6
Dimas Arya Murdiyan <37246423+dimasaryamurdiyan@users.noreply.github.com>
2025-06-18 10:52:42 +0700

    Time: 187 ms (5.3%), Space: 36.8 MB (100%) - LeetHub

---
+++ 0012-integer-to-roman/0012-integer-to-roman.kt
@@ -0,0 +1,11 @@
+class Solution {
+    fun intToRoman(num: Int): String {
+        var number = num
+        val thousands = arrayOf("", "M", "MM", "MMM")
+        val hundreds = arrayOf("", "C","CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM")
+        val tens = arrayOf("", "X", "XX", "XXX", "XL", "L","LX", "LXX", "LXXX", "XC")
+        val units = arrayOf("", "I","II","III", "IV","V", "VI", "VII", "VIII", "IX")
+
+        return thousands[num / 1000] + hundreds[(num % 1000) / 100] + tens[(num % 100) / 10] + units[num % 10]
+    }
+}
