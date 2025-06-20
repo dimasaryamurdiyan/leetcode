6ec06b0f780653f16648c99aee4c435f5302309a
Dimas Arya Murdiyan <37246423+dimasaryamurdiyan@users.noreply.github.com>
2025-06-21 00:49:11 +0700

    Time: 2 ms (98.73%), Space: 47.9 MB (64.22%) - LeetHub

--- 0001-two-sum/0001-two-sum.kt
+++ 0001-two-sum/0001-two-sum.kt
@@ -1,17 +1,16 @@
 class Solution {
     fun twoSum(nums: IntArray, target: Int): IntArray {
-        var output = IntArray(2)
         val hash = hashMapOf<Int, Int>()
 
         for (i in nums.indices) {
             val checkDiff = target - nums[i]
             if (hash.containsKey(checkDiff)) {
-                output = intArrayOf(hash.get(checkDiff) ?: -1, i)
+                val output = intArrayOf(hash.get(checkDiff) ?: -1, i)
                 return output
             }
             hash.put(nums[i], i)
         }
 
-        return output
+        return intArrayOf()
     }
 }
