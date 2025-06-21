fd182f1dedaa6166b3f37e05d624b7df946fac01
Dimas Arya Murdiyan <37246423+dimasaryamurdiyan@users.noreply.github.com>
2025-06-21 14:55:44 +0700

    Time: 16 ms (6.74%), Space: 42.6 MB (44.8%) - LeetHub

--- 0058-length-of-last-word/0058-length-of-last-word.kt
+++ 0058-length-of-last-word/0058-length-of-last-word.kt
@@ -1,7 +1,17 @@
 class Solution {
     fun lengthOfLastWord(s: String): Int {
         val words = s.trim()
-        
-        return words.substringAfterLast(" ").length
+        var counter = 0
+
+        for (i in words.length-1 downTo 0) {
+            println("${words[i].equals(" ")}")
+            if (words[i] == ' ') {
+                return counter
+            } else {
+                counter++
+            }
+        }
+
+        return counter
     }
 }
