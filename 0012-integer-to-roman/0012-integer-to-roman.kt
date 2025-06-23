ef35587e8590741b39222955506a782a47be368c
Dimas Arya Murdiyan <37246423+dimasaryamurdiyan@users.noreply.github.com>
2025-06-23 20:46:50 +0700

    Time: 5 ms (81.03%), Space: 45.3 MB (94.09%) - LeetHub

--- 0012-integer-to-roman/0012-integer-to-roman.kt
+++ 0012-integer-to-roman/0012-integer-to-roman.kt
@@ -1,9 +1,9 @@
 class Solution {
     fun intToRoman(num: Int): String {
-        val ones = listOf("", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX")
-        val tens = listOf("", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC")
-        val hundreds = listOf("", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM")
-        val thousands = listOf("", "M", "MM", "MMM")
+        val ones = arrayOf("", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX")
+        val tens = arrayOf("", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC")
+        val hundreds = arrayOf("", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM")
+        val thousands = arrayOf("", "M", "MM", "MMM")
 
         val builder = StringBuilder()
 
