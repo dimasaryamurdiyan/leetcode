538da87e69c7ca89ab522f9b6f8c8aaf295a5099
Dimas Arya Murdiyan <37246423+dimasaryamurdiyan@users.noreply.github.com>
2025-06-25 18:56:08 +0700

    Time: 3 ms (87.58%), Space: 64.7 MB (87.8%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.kt
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.kt
@@ -1,18 +1,14 @@
 class Solution {
     fun maxProfit(prices: IntArray): Int {
         var profit = 0
-        var buy = 0
-        var sell = 1
-
-        while (sell < prices.size) {
-            if (prices[buy] > prices[sell]) {
-                buy++
-                sell = buy + 1
-            } else {
-                profit = maxOf(profit, prices[sell] - prices[buy])
-
-                sell++
+        var buy = prices[0]
+        
+        for (i in prices.indices) {
+            if (buy > prices[i]) {
+                buy = prices[i]
             }
+
+            profit = maxOf(profit, prices[i] - buy)
         }
 
         return profit
