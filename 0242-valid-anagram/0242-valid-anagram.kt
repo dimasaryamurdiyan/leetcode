038b577be27316f5b2f0eda689f23eaf20fae155
Dimas Arya Murdiyan <37246423+dimasaryamurdiyan@users.noreply.github.com>
2025-06-25 10:54:02 +0700

    Time: 24 ms (41.63%), Space: 46.7 MB (28.47%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.kt
+++ 0242-valid-anagram/0242-valid-anagram.kt
@@ -1,8 +1,26 @@
 class Solution {
     fun isAnagram(s: String, t: String): Boolean {
-        val strS = String(s.toCharArray().apply { sort() })
-        val strT = String(t.toCharArray().apply { sort() })
+        if (s.length != t.length) return false
 
-        return strS == strT
+        val tempChar = hashMapOf<Char, Int>()
+        for (char in s) {
+            tempChar.put(char, 1 + tempChar.getOrDefault(char, 0))
+        }
+
+        for (char in t) {
+            if (!tempChar.contains(char)) {
+                return false
+            } else {
+                if (tempChar.get(char) != null) {
+                    tempChar.put(char, tempChar.getOrDefault(char, 0) - 1)
+                }
+
+                if (tempChar.get(char) == 0) {
+                    tempChar.remove(char)
+                }
+            }
+        }
+
+        return tempChar.isEmpty()
     }
 }
