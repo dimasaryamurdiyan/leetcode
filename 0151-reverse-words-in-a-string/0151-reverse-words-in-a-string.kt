f911f1d32aaebc8aba3f9fc6b3bb9b69f8b8af46
Dimas Arya Murdiyan <37246423+dimasaryamurdiyan@users.noreply.github.com>
2025-07-03 03:51:45 +0700

    Time: 20 ms (65.03%), Space: 46 MB (51.37%) - LeetHub

--- 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.kt
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.kt
@@ -1,15 +1,7 @@
 class Solution {
     fun reverseWords(s: String): String {
-        val words = s.replace("\\s+".toRegex(), " ").trim().split(" ")
-
-        val builder = StringBuilder()
-        for (i in words.size-1 downTo 0) {
-            builder.append(words[i])
-            if (i > 0) {
-                builder.append(" ")
-            }
-        }
-
-        return builder.toString()
+        val words = s.trim().split("\\s+".toRegex())
+        val reversed = words.reversed()
+        return reversed.joinToString(" ")
     }
 }
