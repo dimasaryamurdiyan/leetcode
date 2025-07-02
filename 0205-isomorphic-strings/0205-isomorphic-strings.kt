53bfe4614600ce08f678d70aa272443635688c0f
Dimas Arya Murdiyan <37246423+dimasaryamurdiyan@users.noreply.github.com>
2025-07-02 22:22:48 +0700

    Time: 16 ms (55.83%), Space: 43.7 MB (36.2%) - LeetHub

---
+++ 0205-isomorphic-strings/0205-isomorphic-strings.kt
@@ -0,0 +1,17 @@
+class Solution {
+    fun isIsomorphic(s: String, t: String): Boolean {
+        val hash = hashMapOf<Char, Char>()
+
+        for (i in s.indices) {
+            if (hash.containsKey(s[i])) {
+                if (hash.get(s[i]) != t[i]) return false
+            } else {
+                if (hash.containsValue(t[i])) return false 
+            }
+
+            hash.put(s[i], t[i])
+        }
+
+        return true
+    }
+}
